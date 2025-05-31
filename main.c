77501a2d7dd70de7c89ee3a02b320536d911465c
ChristianHopf <cehopf@gmail.com>
2025-05-31 14:41:48 +0000

    first scene_render impl

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <cglm/mat4.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
@@ -45,7 +46,40 @@ bool firstMouse = true;
 float lastX = 400.0f;
 float lastY = 300.0f;
 
-Engine engine;
+// Move to a scene.c when done
+void scene_render(Scene *scene){
+  // Render (clear color and depth buffer bits)
+  glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  // Get view and projection matrices
+  mat4 view;
+  mat4 projection;
+  camera_get_view_matrix(scene->camera, view);
+  glm_perspective(glm_rad(scene->camera->fov), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f, projection);
+
+  // For each entity in the scene
+  for(int i = 0; i < scene->num_entities; i++){
+    // Bind its shader
+    Entity *entity = &scene->entities[i];
+    shader_use(entity->shader->ID);
+
+    // Get its model matrix
+    mat4 model;
+    glm_mat4_identity(model);
+    glm_translate(model, (vec3){0.0f, 0.0f, 0.0f});
+    glm_scale(model, (vec3){1.0f, 1.0f, 1.0f});
+    shader_set_mat4(&shader, "model", model);
+
+    // Set its model, view, and projection matrix uniforms
+    shader_set_mat4(&shader, "model", model);
+    shader_set_mat4(&shader, "view", view);
+    shader_set_mat4(&shader, "projection", projection);
+
+    // Draw model
+    model_draw(entity->model, &shader);
+  }
+}
 
 void processInput(GLFWwindow *window){
 	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
