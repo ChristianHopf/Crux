ec4108defc07b054b5eacf3b0f3eb910c546a8c4
ChristianHopf <cehopf@gmail.com>
2025-05-31 19:54:31 +0000

    new model struct

--- include/newmodel.h
+++ include/newmodel.h
@@ -2,5 +2,15 @@
 #define NEWMODEL_H
 
 #include <cJSON/cJSON.h>
+#include "mesh.h"
+
+typedef struct {
+  const char *path;
+  unsigned char *data;
+  cJSON *json;
+} NewModel;
+
+Model *model_create(const char *path);
+void model_draw(Shader *shader, Camera *camera);
 
 #endif
