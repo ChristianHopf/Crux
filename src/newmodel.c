16e9ea92a7287f9adf9faffd8d1a0509788997f9
ChristianHopf <cehopf@gmail.com>
2025-06-01 00:24:43 +0000

    process mesh

--- src/newmodel.c
+++ src/newmodel.c
@@ -26,7 +26,7 @@ bool model_load(Model *model, const char *path){
   model->num_meshes = scene->mNumMeshes;
   
   // Process all meshes
-  for(unsigned int i = 0; i < scene->mNumMeshes; ++i){
+  for(unsigned int i = 0; i < scene->mNumMeshes; i++){
     model_process_mesh(scene->mMeshes[i], &model->meshes[i]);
   }
 
@@ -40,14 +40,61 @@ void model_process_node(Model *model, struct aiNode *node, struct aiScene *scene
 
 void model_process_mesh(struct aiMesh *ai_mesh, Mesh *dest_mesh){
   // Allocate vertices and indices
+  Vertex *vertices = (Vertex *)malloc(ai_mesh->mNumVertices, sizeof(Vertex));
+  if (!vertices){
+    printf("Error: failed to allocate vertices in model_process_mesh\n");
+  }
+  unsigned int *indices = (unsigned int *)malloc(ai_mesh->mNumFaces * 3 * sizeof(unsigned int));
+  if (!indices){
+    printf("Error: failed to allocate indices in model_process_mesh\n");
+  }
 
   // Process vertices
+  for (unsigned int i = 0; i < ai_mesh->mNumVertices; i++){
+    // Copy position vertices to vertex position vector
+    memcpy(vertices[i].position, &ai_mesh->mVertices[i], sizeof(float) * 3);
+    if (ai_mesh->HasNormals()){
+      memcpy(vertices[i].normal, &ai_mesh->mNormals[i], sizeof(float) * 3);
+    }else{
+      memset(vertices[i].normal, 0, sizeof(float) * 3);
+    }
+  }
 
   // Process indices
+  unsigned int index = 0;
+  for(unsigned int i = 0; i < ai_mesh->mNumFaces; i++){
+    struct aiFace face = ai_mesh->mFaces[i];
+    for(unsigned int j = 0; j < face.mNumIndices; j++){
+      indices[index++] = face.mIndices[j];
+    }
+  }
+  mesh->num_indices = index;
 
   // Bind vertex buffers and buffer vertex data
+  glGenBuffers(1, &mesh->VBO);
+  glGenBuffers(1, &mesh->EBO);
+  glGenVertexArrays(1, &mesh->VAO);
+
+  // Bind vertex array
+  glBindVertexArray(&mesh->VAO);
 
   // Bind element buffers and buffer indices data
+  glBindBuffer(GL_ARRAY_BUFFER, mesh->VBO);
+  glBufferData(GL_ARRAY_BUFFER, ai_mesh->mNumvertices * sizeof(Vertex), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh->EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, index * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
   // Configure attribute pointers
+  // Position
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+  // Normal
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(3 * sizeof(float)));
+
+  glBindVertexArray(0);
+
+  free(vertices);
+  free(indices);
 }
