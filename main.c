15e4aeb651ac88bc556dd4a452bdbf6aacb4d5f6
ChristianHopf <cehopf@gmail.com>
2025-06-07 04:45:48 +0000

    Blending

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include <cglm/mat4.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
@@ -162,6 +161,9 @@ Engine *engine_create(){
 	glEnable(GL_DEPTH_TEST);
   glEnable(GL_CULL_FACE);
   glEnable(GL_FRAMEBUFFER_SRGB);
+
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
   //glDepthFunc(GL_ALWAYS);
   //glEnable(GL_STENCIL_TEST);
