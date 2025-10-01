931c2cd0c2efdb47e2db6cfbc7708a3fe7dfefb9
ChristianHopf <cehopf@gmail.com>
2025-10-01 14:00:32 -0400

    Header defs for custom event callbacks and registry

--- include/event.h
+++ include/event.h
@@ -5,6 +5,9 @@
 #include "time.h"
 #include "types.h"
 
+#define MAX_EVENT_TYPES 64
+#define MAX_LISTENERS_PER_TYPE 32
+
 typedef enum {
   EVENT_COLLISION = 0,
   EVENT_PLAYER_ITEM_PICKUP,
@@ -24,8 +27,21 @@ struct GameEvent {
       int item_count;
       uuid_t item_entity_id;
     } item_pickup;
+    void *custom;
   } data;
 };
+
+typedef void (*EventCallback)(struct GameEvent *game_event, void *user_data);
+
+struct EventListener {
+  EventCallback callback;
+  void *user_data;
+};
+
+struct EventRegistry {
+  struct EventListener listeners[MAX_EVENT_TYPES][MAX_LISTENERS_PER_TYPE];
+  int listener_counts[MAX_EVENT_TYPES];
+}
 
 struct GameEventQueue {
   struct GameEvent *events;
@@ -34,6 +50,7 @@ struct GameEventQueue {
   int back;
   int size;
   struct Scene *scene;
+  struct EventRegistry event_registry;
 };
 
 
@@ -50,3 +67,7 @@ bool game_event_queue_is_empty();
 void game_event_queue_process();
 EventType get_event_type(EntityType type_A, EntityType type_B);
 void game_event_print(struct GameEvent *game_event);
+
+// Listeners
+void event_listener_register(EventType type, EventCallback callback, void *user_data);
+void event_listener_unregister(EventType type, EventCallback callback);
