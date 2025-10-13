526dbfe9f00b4616c609024c901b6f890efc83d3
ChristianHopf <cehopf@gmail.com>
2025-10-13 17:09:06 -0400

    Header and first functions impl for objectives

---
+++ include/objective.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <stdbool.h>
+
+#define MAX_OBJECTIVES 32
+
+typedef enum {
+  OBJECTIVE_COLLECT_ITEM
+} ObjectiveType;
+
+struct Objective {
+  ObjectiveType type;
+  union {
+    struct {
+      int item_id;
+      int required_count;
+      int current_count;
+    } collect_item;
+  } data;
+  bool complete;
+  void *user_data;
+};
+
+struct ObjectiveManager {
+  struct Objective objectives[MAX_OBJECTIVES];
+  unsigned int num_objectives;
+};
+
+
+bool objective_manager_init(struct ObjectiveManager *objective_manager);
+void objective_manager_update_on_event(struct ObjectiveManager *objective_manager, struct GameEvent *game_event);
+void objective_manager_destroy(struct ObjectiveManager *objective_manager);
+void objective_manager_objective_add(struct ObjectiveManager *objective_manager, struct Objective objective);
---
+++ src/objective.c
@@ -0,0 +1,47 @@
+#include "objective.h"
+#include "event.h"
+
+bool objective_manager_init(struct ObjectiveManager *objective_manager){
+  memset(objective_manager, 0, sizeof(struct ObjectiveManager));
+
+  return true;
+}
+
+void objective_manager_update_on_event(struct ObjectiveManager *objective_manager, struct GameEvent *game_event){
+  // For each objective, if it isn't complete, switch on its ObjectiveType
+  // and check if this event satisfies its conditions
+  for (int i = 0; i < objective_manager->num_objectives; i++){
+    struct Objective *objective = &objective_manager->objectives[i];
+    if (objective->completed) continue;
+
+    switch (objective->type){
+      case OBJECTIVE_COLLECT_ITEM: {
+        if (game_event->type == EVENT_PLAYER_ITEM_PICKUP){
+          if (game_event->data.item_pickup.item_id == objective->data.collect_item.item_id){
+            objective->data.collect_item.current_count += game_event->data.item_pickup.item_count;
+            if (objective->data.collect_item.current_count >= objective->data.collect_item.required_count){
+              objective->complete = true;
+              printf("Objective complete!\n");
+            }
+          }
+        }
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+}
+
+void objective_manager_destroy(struct ObjectiveManager *objective_manager){
+}
+
+void objective_manager_objective_add(struct ObjectiveManager *objective_manager, struct Objective objective){
+  if (!objective_manager || objective_manager->num_objectives >= MAX_OBJECTIVES){
+    fprintf(stderr, "Error: failed to add objective, either ObjectiveManager is invalid or max objectives reached\n");
+    return;
+  }
+
+  objective_manager->objectives[objective_manager->num_objectives++] = objective;
+}
